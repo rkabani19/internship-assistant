c5f8715b23babf6a2e2f73d164ba6273ad1eb943
Rohail Kabani <rohail.kabani@gmail.com>
2020-06-27 13:36:26 -0400

    Setup company list (#5)
    * add map of companies mapping to their career/internship urls

---
+++ internship/internship.go
@@ -0,0 +1,14 @@
+package internship
+
+companies := map[string]string {
+	"Square": "https://squareup.com/ca/careers/jobs?type=Intern",
+	"Slack": "https://slack.com/intl/en-ca/careers/university-recruiting#openings",
+	"Stripe": "https://stripe.com/jobs/search?s=intern",
+	"Lyft": "https://www.lyft.com/careers/university",
+	"Uber": "https://www.uber.com/us/en/careers/teams/university/",
+	"Credit Karma": "https://www.creditkarma.com/careers/jobs/university",
+	"Airbnb": "https://careers.airbnb.com/university/",
+	"Pinterest": "https://www.pinterestcareers.com/university",
+	"Mozilla": "https://careers.mozilla.org/listings/?position_type=Intern",
+	"Palantir": "https://www.palantir.com/careers/",
+}
