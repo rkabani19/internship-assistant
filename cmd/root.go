fbe3e0cc3de1cf3e1615332c00ed7d84d235dece
rkabani19 <rohail.kabani@gmail.com>
2020-07-17 17:49:37 -0400

    config err

--- cmd/root.go
+++ cmd/root.go
@@ -51,9 +51,7 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	} else {
-		fmt.Printf("%v\n", err)
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Unable to find config file")
 	}
 }
